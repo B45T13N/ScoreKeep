2a828aebf1321f737bb92021082883f2fb21f432
B45T13N <bastds.dev@gmail.com>
2023-06-06 20:38:15 +0200

    Update datas fields to match with API Rules

--- ScoreKeep/Business/Models/Interface/Person.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ScoreKeep.Business.Models.Interface;
-
-public interface Person
-{
-    public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Email { get; set; }
-}
--- ScoreKeep/Data/LocalTeam.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ScoreKeep.Data;
-
-public class LocalTeam
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string? Logo { get; set; }
-
-}
