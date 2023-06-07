346c67e6baea92a00cea9d05adb3792731724136
B45T13N <bastds.dev@gmail.com>
2023-06-07 17:39:12 +0200

    Add localteam details

--- ScoreKeep/Business/Models/Game.cs
+++ ScoreKeep/Business/Models/Game.cs
@@ -5,6 +5,7 @@ public class Game
     public int Id { get; set; }
     public string Address { get; set; }
     public string Category { get; set; }
+    public LocalTeam LocalTeam { get; set; }
     public DateTime GameDate { get; set; }
     public Timekeeper Timekeeper { get; set; }
     public Secretary Secretary { get; set; }
---
+++ ScoreKeep/Business/Models/LocalTeam.cs
@@ -0,0 +1,8 @@
+﻿namespace ScoreKeep.Business.Models;
+
+public class LocalTeam
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Logo { get; set; }
+}
