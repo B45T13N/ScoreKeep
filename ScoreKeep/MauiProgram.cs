1f33b862bca28ecd582ad66c07d4a2c60beea7e8
B45T13N <bastds.dev@gmail.com>
2023-08-05 01:12:18 +0200

    clean code

--- ScoreKeep/MauiProgram.cs
+++ ScoreKeep/MauiProgram.cs
@@ -13,8 +13,9 @@ public static class MauiProgram
             .UseMauiApp<App>()
             .ConfigureFonts(fonts =>
             {
-                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("Barlow-Regular.ttf", "Barlow");
+                fonts.AddFont("Barlow-SemiBoldItalic.ttf", "BarlowSemiBoldItalic");
+                fonts.AddFont("Barlow-SemiBold.ttf", "BarlowSemiBold");
             })
             .UseMauiCommunityToolkit();
 
