c757784c91fd7e3ddee8a0b33101044faaa3e445
B45T13N <87314220+B45T13N@users.noreply.github.com>
2023-08-19 10:59:18 +0200

    Feature/5 setup home page to be local team view (#9)
    * Feature/1 register colors from figma (#2) (#3)
    
    * Update fonts and add definitive fonts
    
    * Update colors and styles to fit with the figma design
    
    * Remove unused styles and clean code
    
    * Remove unused styles and clean code
    
    * Add to upper function on page title
    
    * clean code
    
    * clean code
    
    * Add svg heart image
    
    * Add gamesindexpage route and setup LocalTeamsIndexPage to be mainpage
    
    * Remove unused image and add heart svg image
    
    * Add notify property into localteam class
    
    * Rename classes

---
+++ ScoreKeep/View/LocalTeamsIndexPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using ScoreKeep.ViewModel;
+
+namespace ScoreKeep.View;
+
+public partial class LocalTeamsIndexPage : ContentPage
+{
+    public LocalTeamsIndexPage(LocalTeamsViewModel viewModel)
+    {
+        InitializeComponent();
+        BindingContext = viewModel;
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        if (BindingContext is LocalTeamsViewModel viewModel)
+        {
+            viewModel.UpdateLocalTeamsAsync();
+            LocalTeamCollection.ItemsSource = viewModel.AllLocalTeams;
+            Connectivity.ConnectivityChanged += viewModel.ConnectivityChanged;
+        }
+    }
+
+    protected override void OnDisappearing()
+    {
+        if (BindingContext is LocalTeamsViewModel viewModel)
+        {
+            Connectivity.ConnectivityChanged -= viewModel.ConnectivityChanged;
+        }
+    }
+
+}
+
---
+++ ScoreKeep/ViewModel/LocalTeamsViewModel.cs
@@ -0,0 +1,116 @@
+﻿using CommunityToolkit.Mvvm.Input;
+
+namespace ScoreKeep.ViewModel;
+
+public partial class LocalTeamsViewModel : BaseViewModel
+{
+    private readonly ILocalTeamService _localTeamService;
+
+    public ObservableCollection<LocalTeam> AllLocalTeams { get; set; } = new();
+
+    public LocalTeamsViewModel(ILocalTeamService localTeamService)
+    {
+        this._localTeamService = localTeamService;
+
+        Title = "Scorekeep".ToUpper();
+
+    }
+
+    private async Task LoadLocalTeamsAsync()
+    {
+        AllLocalTeams.Clear();
+        try
+        {
+            IsBusy = true;
+            var localTeams = await _localTeamService.GetLocalTeamsAsync();
+
+            if (localTeams.Count == 0)
+            {
+                ErrorMessage = "Aucune équipe trouvée.";
+                IsErrorVisible = true;
+            }
+            else
+            {
+                foreach (var localTeam in localTeams)
+                {
+                    var updatedLocalTeam = CheckIfLocalTeamIsAlreadyLiked(localTeam);
+                    AllLocalTeams.Add(updatedLocalTeam);
+                }
+
+                IsErrorVisible = false;
+            }
+
+        }
+        catch (Exception e)
+        {
+            ErrorMessage = "Une erreur est survenue lors du chargement des équipes.";
+            IsErrorVisible = true;
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
+
+    public void UpdateLocalTeamsAsync()
+    {
+        _ = LoadLocalTeamsAsync();
+    }
+
+    public void ConnectivityChanged(object sender, ConnectivityChangedEventArgs e)
+    {
+        if (e.NetworkAccess == NetworkAccess.Internet)
+        {
+            _ = LoadLocalTeamsAsync();
+            ErrorMessage = "";
+            IsErrorVisible = false;
+        }
+        else
+        {
+            AllLocalTeams.Clear();
+            ErrorMessage = "La connexion internet a été perdue";
+            IsErrorVisible = true;
+        }
+    }
+
+    [RelayCommand]
+    public async Task GotToGameListAsync(LocalTeam localTeam)
+    {
+        if (localTeam is null)
+            return;
+
+        await Shell.Current.GoToAsync($"{nameof(GamesIndexPage)}", true, new Dictionary<string, object>
+        {
+            { "LocalTeam", localTeam}
+        });
+    }
+
+    [RelayCommand]
+    public void HandleHeartButtonClick(LocalTeam localTeam)
+    {
+        localTeam.IsLiked = !localTeam.IsLiked;
+
+        if (localTeam.IsLiked)
+        {
+            Preferences.Set(localTeam.Id.ToString(), localTeam.Name);
+        }
+        else
+        {
+            Preferences.Remove(localTeam.Id.ToString());
+        }
+    }
+
+    private LocalTeam CheckIfLocalTeamIsAlreadyLiked(LocalTeam localTeam)
+    {
+        if (Preferences.Get(localTeam.Id.ToString(), "") != "")
+        {
+            localTeam.IsLiked = true;
+        }
+        else
+        {
+            localTeam.IsLiked = false;
+        }
+
+        return localTeam;
+    }
+}
