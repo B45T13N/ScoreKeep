b6bb3683df15f3c7819a88301c3e5b36e21b06c0
B45T13N <bastds.dev@gmail.com>
2023-08-30 16:58:11 +0200

    Revert "Add custom exception"
    This reverts commit 98a87e7fe78e36fde9d40bf35b1ff96ae1b21472.

--- ScoreKeep/Business/Providers/API.cs
+++ ScoreKeep/Business/Providers/API.cs
@@ -2,7 +2,7 @@
 
 public static class API
 {
-    public static string URI = "https://api-score-keep.bds-dev.fr";
-    public static string APIKey = "mfevFIuk3TBCq619LTttzEd0joOkO1YxRPC5G4RQ8tY=";
+    public static string URI = "";
+    public static string APIKey = "";
 }
 
--- ScoreKeep/Exceptions/ConnexionException.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ScoreKeep.Exceptions;
-public class ConnexionException : Exception
-{
-    public ConnexionException(string message) : base(message)
-    {
-    }
-}
-
