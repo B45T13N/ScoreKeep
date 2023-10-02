5e82deae11474c4acb5e5dd5c2a499fa903c8d8b
B45T13N <bastds.dev@gmail.com>
2023-10-02 16:07:35 +0200

    Add exception on Volunteer type not found

---
+++ ScoreKeep/Exceptions/VolunteerTypeNotFound.cs
@@ -0,0 +1,11 @@
+﻿namespace ScoreKeep.Exceptions;
+public class VolunteerTypeNotFound : Exception
+{
+    public VolunteerTypeNotFound() : base()
+    {
+    }
+    public VolunteerTypeNotFound(string message) : base(message)
+    {
+    }
+}
+
